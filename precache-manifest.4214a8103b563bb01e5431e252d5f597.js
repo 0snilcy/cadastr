02d0a0c703d926729606ba2ace8d1aec85bc2eeb
Johnny Rocket <0.snilcy@gmail.com>
2020-08-19 16:04:10 +0300

    Updates

---
+++ precache-manifest.4214a8103b563bb01e5431e252d5f597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a05f504a0f49ba33c64e8a4ee7240bb",
+    "url": "/cadastr/index.html"
+  },
+  {
+    "revision": "d5d890f95963152a9f49",
+    "url": "/cadastr/static/css/main.df553946.chunk.css"
+  },
+  {
+    "revision": "2e29a02676d6291e12c8",
+    "url": "/cadastr/static/js/2.2343548b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cadastr/static/js/2.2343548b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d890f95963152a9f49",
+    "url": "/cadastr/static/js/main.8356b113.chunk.js"
+  },
+  {
+    "revision": "41aef8a43f270db2889b",
+    "url": "/cadastr/static/js/runtime-main.43f93367.js"
+  }
+]);
