48697ff75a4f67a057e3109e00fabcdea559a9ec
Johnny Rocket <0.snilcy@gmail.com>
2020-08-21 14:09:36 +0300

    Change copy logic

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -10,10 +10,48 @@ const ContentItem = ({ CADNOMER, ADDRESS }) => {
   );
 };
 
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement('textarea');
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = '0';
+  textArea.style.left = '0';
+  textArea.style.position = 'fixed';
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(
+    function () {
+      console.log('Async: Copying to clipboard was successful!');
+    },
+    function (err) {
+      console.error('Async: Could not copy text: ', err);
+    }
+  );
+}
+
 const copyItems = async (value) => {
   try {
-    await navigator.clipboard.writeText(value);
-    console.log('Copied');
+    copyTextToClipboard(value);
   } catch (err) {
     console.log(err);
   }
